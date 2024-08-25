ea6b218b8a5f1e8e48dc0e9da5e1e55c97727af3
Abdusharif0095 <abdurafiki.abdusharif@gmail.com>
2024-08-25 23:08:03 +0300

    initial commit

---
+++ repository.py
@@ -0,0 +1,25 @@
+from database import new_session
+from schemas import STaskAdd, STask
+from database import TaskOrm
+from sqlalchemy import select
+
+class TaskRepository:
+    @classmethod
+    async def add_one(cls, data: STaskAdd) -> int:
+        async with new_session() as session:
+            task_dict = data.model_dump()
+
+            task = TaskOrm(**task_dict)
+            session.add(task)
+            await session.flush()
+            await session.commit()
+            return task.id
+
+    @classmethod
+    async def get_all(cls) -> STask:
+        async with new_session() as session:
+            query = select(TaskOrm)
+            result = await session.execute(query)
+            task_models = result.scalars().all()
+            task_schemas = [STask.model_validate for task in task_models]
+            return task_schemas
